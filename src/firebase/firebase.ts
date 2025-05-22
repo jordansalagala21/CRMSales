14ba44878006f6415419818e851c00216a2a187b
jordansalagala21 <officjordansal@gmail.com>
2025-05-22 08:00:25 -0400

    added environment variable and analytics page

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBsMrz-2JdNTQTynoMit4g4wVWHUbjfkPs",
-  authDomain: "admindashboard-f3d44.firebaseapp.com",
-  projectId: "admindashboard-f3d44",
-  storageBucket: "admindashboard-f3d44.firebasestorage.app",
-  messagingSenderId: "313657688635",
-  appId: "1:313657688635:web:826ed6dd6a1bad2215921e",
-  measurementId: "G-LWTZ5G85VC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
